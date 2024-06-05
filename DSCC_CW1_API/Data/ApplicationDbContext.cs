7505b65f1c3feea82ca2a916e3500521cac5d720
wiut-std <jjumaniyozov@students.wiut.uz>
2024-06-05 21:13:40 +0500

    ApplicationDb context

---
+++ DSCC_CW1_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using DSCC_CW1_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSCC_CW1_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuring the Author entity
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.HasKey(e => e.AuthorId);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Bio)
+                    .HasMaxLength(250);
+            });
+
+            // Configuring the Book entity
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.BookId);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.YearPublished);
+
+                entity.HasOne(d => d.Author)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.AuthorId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
