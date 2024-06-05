d1a0a88c299ed41fa8156667496bc5df39fb7a0a
wiut-std <jjumaniyozov@students.wiut.uz>
2024-06-05 21:13:29 +0500

    Interfaces added

---
+++ DSCC_CW1_API/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,14 @@
+﻿using DSCC_CW1_API.Dtos;
+using DSCC_CW1_API.Models;
+
+namespace DSCC_CW1_API.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        Task<IEnumerable<Author>> GetAllAuthorsAsync();
+        Task<Author> GetAuthorByIdAsync(int authorId);
+        Task<Author> CreateAuthorAsync(Author author);
+        Task<Author> UpdateAuthorAsync(Author author);
+        Task DeleteAuthorAsync(int authorId);
+    }
+}
---
+++ DSCC_CW1_API/Interfaces/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using DSCC_CW1_API.Dtos;
+using DSCC_CW1_API.Models;
+
+namespace DSCC_CW1_API.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllBooksAsync();
+        Task<Book> GetBookByIdAsync(int bookId);
+        Task<Book> CreateBookAsync(Book book);
+        Task<Book> UpdateBookAsync(Book book);
+        Task DeleteBookAsync(int bookId);
+    }
+}
