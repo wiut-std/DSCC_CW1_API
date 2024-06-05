cc6a55464d29c6a4b0981cad0332f2bd362c2bfe
wiut-std <jjumaniyozov@students.wiut.uz>
2024-06-05 21:12:59 +0500

    Models developed

---
+++ DSCC_CW1_API/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace DSCC_CW1_API.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        // Navigation property
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ DSCC_CW1_API/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace DSCC_CW1_API.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int YearPublished { get; set; }
+
+        // Foreign key
+        public int AuthorId { get; set; }
+
+        // Navigation property
+        public Author Author { get; set; }
+    }
+}
